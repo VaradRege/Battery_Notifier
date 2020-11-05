450b9d6c37ff10d6e3bda3266e0ca70fabca6b38
AYUSHJD098 <66873088+AYUSHJD098@users.noreply.github.com>
2020-11-05 19:55:44 +0530

    Made Notifier Actully usable
    Accept this this improves notifier by 1000% times

--- Battery_notifier.py
+++ Battery_notifier.py
@@ -1,42 +1,65 @@
-# pip install py-notifier
+# Python3 Document 
+#project by VaradRege aka Philoshopher 
+# Edit By Ayushjd098
+#!/usr/bin/env python
+import signal
+import sys
 from pynotifier import Notification
+import time
+import psutil 
 
-# pip install psutil
-import psutil
+def notify():
 
-# psutil.sensors_battery() will return the information related to the battery
-battery = psutil.sensors_battery()
+	battery = psutil.sensors_battery()
 
-# battery.percent will return the current battery percentage
-percent = battery.percent
+	old_bat = 0 
 
-# Notification(title, description, duration) -- to send notification to desktop
-Notification("Battery Percentage",
-             str(percent)+ "% Percent Remaining",
-             duration=10).send()
+	while True:
+		signal.signal(signal.SIGINT, signal_handler)
+# For Future Refrence
+# signal.pause()
+# time.sleep(900)
 
+		percent = battery.percent
 
+		if percent < old_bat: 
+			old_bat = percent - 10
+			if percent < 20:
+				Notification("Battery Level Reached Critical",
+	             str(percent)+ "% Percent Remaining, Please Provide Power.",
+	             duration=20).send()
 
+			else:
+				Notification("Battery Level Going Down",
+				             str(percent)+ "% Percent Remaining",
+				             duration=10).send()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		if percent > old_bat:
+				Notification("Battery Stable :)",
+				             str(percent)+ "% Percent Remaining",
+				             duration=5).send()		
+		old_bat = percent
+		time.sleep(900)
 
 
 
+def signal_handler(sig, frame):
+	print('Exiting Gracefully...')
+	print("Thanks For Using me")
+	print("For more info visit: https://github.com/VaradRege/Battery_Notifier")
+	sys.exit(0)
+	
+def starting_screen():
+	print("*" * 50)
+	print(" ")
+	print("Hey,")
+	print("This program is gonna moniter your Battery")
+	print("By useing it you  accept our Terms & Condiations!")
+	print("To stop press Ctrl + C ")
+	print(" ")
+	print("*" * 50)
+
+
+if __name__ == '__main__':
+	starting_screen()
+	notify()
